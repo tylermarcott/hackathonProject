9c268a1aef8f24a4a3098086ba9f3cbec47541c0
everettsmith928 <everett.marsland.smith@gmail.com>
2023-09-08 14:29:55 -0600

    html update

--- hackathonProject.client/app/controllers/DogsController.js
+++ hackathonProject.client/app/controllers/DogsController.js
@@ -1,24 +1,28 @@
-import { dogsService } from "../services/DogsService"
-import { Pop } from "../utils/Pop"
+import { dogsService } from "../services/DogsService.js"
+import { Pop } from "../utils/Pop.js"
+
+
+
+
 
 export class DogsController {
   constructor() {
     console.log('This is the thing Dogs')
   }
 
-  // async getDogs() {
-  //   try {
-  //     const dogs = await dogsService.getDogs()
-  //   } catch (error) {
-  //     Pop.error(error)
-  //   }
-  // }
+  async getDogs() {
+    try {
+      // const dogs = await dogsService.getDogs()
+    } catch (error) {
 
-  // async createDog() {
-  //   try {
-  //     const newDog = await dogsService.createDog()
-  //   } catch (error) {
-  //     Pop.error(error)
-  //   }
-  // }
+    }
+  }
+
+  async createDog() {
+    try {
+      const newDog = await dogsService.createDog()
+    } catch (error) {
+      Pop.error(error)
+    }
+  }
 }
--- hackathonProject.client/app/services/DogsService.js
+++ hackathonProject.client/app/services/DogsService.js
@@ -1,7 +1,9 @@
+import { AppState } from "../AppState"
+
 class DogsService {
-
   async createDog() {
-
+    const dog = AppState.dog.
   }
 }
+
 export const dogsService = new DogsService()
