3175fe52f61194f02f51fb58ee6dcbea86fc4bb8
Lincoln Marler <141679750+lincmarler@users.noreply.github.com>
2023-09-08 14:24:36 -0600

    Get function

--- hackathonProject/server/services/DogService.js
+++ hackathonProject/server/services/DogService.js
@@ -2,6 +2,10 @@ import { dbContext } from "../db/DbContext.js"
 
 
 class DogService {
+    async getDogs() {
+        const dogs = await dbContext.Dogs.find().populate('Account')
+        return dogs
+    }
     async createDog(body) {
         const newDog = await dbContext.Dogs.create(body)
         return newDog
