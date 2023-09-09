7c97518e93bfa7e7a23286acffac147a916b2e37
tif <youfoundtiffany@gmail.com>
2023-09-08 22:03:34 -0600

    dog watchers are complete

--- hackathonProject/server/services/DogWatchersService.js
+++ hackathonProject/server/services/DogWatchersService.js
@@ -2,6 +2,17 @@ import { dbContext } from "../db/DbContext.js"
 import { BadRequest, Forbidden } from "../utils/Errors.js"
 
 class DogWatcherService {
+    async removeDogWatcher(watcherId, userInfo) {
+        const dogWatcherToRemove = await dbContext.Watchers.findById(watcherId).populate('dog')
+        if (!dogWatcherToRemove) throw new BadRequest('no dog watcher with id: ${watcherId}')
+
+        if (dogWatcherToRemove.accountId != userInfo.id) throw new Forbidden("You don't own this, can't delete.")
+
+        await dogWatcherToRemove.remove()
+        // @ts-ignore
+        return `you have unseen the ${dogWatcherToRemove.dog.name}`
+
+    }
     async getDogWatcher(query) {
         const watcher = await dbContext.Watchers.find(query).populate('profile dog')
         return watcher
