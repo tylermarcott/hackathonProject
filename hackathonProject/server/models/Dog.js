20d1384a1cda375618b2904e398260ea0ab46163
Tyler Marcott <tylerjmarcott@gmail.com>
2023-09-08 19:41:05 -0600

    working on dogs?

--- hackathonProject/server/models/Dog.js
+++ hackathonProject/server/models/Dog.js
@@ -5,6 +5,7 @@ import { Schema } from "mongoose";
 export const DogSchema = new Schema({
     name: { type: String, required: true, minlength: 1, maxlength: 50 },
     type: { type: Boolean, required: true, default: true },
+    imgUrl: { type: String, maxlength: 1000 },
     powers: { type: String, required: true, minlength: 2, maxlength: 50 },
     description: { type: String, maxlength: 500 },
     accountId: { type: Schema.Types.ObjectId, ref: 'Account', required: true }
